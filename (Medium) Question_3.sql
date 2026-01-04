ae74fe167a4ecb5bcccbbfee71bfab3462182143
Phuong (Joey) Le <lethuphuong2812@gmail.com>
2026-01-04 17:06:33 +0000

    Add files via upload

---
+++ (Medium) Question_3.sql
@@ -0,0 +1,33 @@
+/*
+Query an alphabetically ordered list of all names in OCCUPATIONS, immediately followed by the first letter of each profession as a parenthetical (i.e.: enclosed in parentheses). For example: AnActorName(A), ADoctorName(D), AProfessorName(P), and ASingerName(S).
+
+Query the number of ocurrences of each occupation in OCCUPATIONS. Sort the occurrences in ascending order, and output them in the following format:
+
+There are a total of [occupation_count] [occupation]s.
+
+where [occupation_count] is the number of occurrences of an occupation in OCCUPATIONS and [occupation] is the lowercase occupation name. If more than one 
+
+Occupation has the same [occupation_count], they should be ordered alphabetically.
+*/
+
+WITH Name_Occupation AS
+(
+    SELECT 
+        name,
+        CONCAT (name, '(', LEFT(Occupation,1), ')') AS name_and_occupation
+    FROM OCCUPATIONS
+)
+-- Result 1
+SELECT 
+    name_and_occupation
+FROM Name_Occupation
+ORDER BY name 
+;
+
+-- Result 2:
+SELECT 
+    CONCAT("There are a total of", " ", COUNT(*), " ", LOWER(Occupation),"s", ".")
+FROM OCCUPATIONS
+GROUP BY Occupation
+ORDER BY COUNT(*) ASC , LOWER(Occupation) ASC
+; 
